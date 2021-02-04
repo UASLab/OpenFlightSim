b4f841b9446e1d9c1449db9fa857e50d81bb5b58
Chris Regan <rega0051@umn.edu>
2021-02-03 22:24:22 -0600

    Adding the F450.ac model back in, somehow the F450.wrl got squashed out of existance. F450.ac is actually an UltraStick25e and looks nothing like a F450!

--- Simulation/python/Example_AIL_F450.py
+++ Simulation/python/Example_AIL_F450.py
@@ -28,7 +28,7 @@ from JSBSimWrapper import JSBSimWrap
 #%% Define Controllers
 import control
 
-tFrameRate_s = 1/50 # Desired Run rate
+tFrameRate_s = 1/200 # Desired Run rate
 
 def PID2(Kp = 1, Ki = 0.0, Kd = 0, b = 1, c = 1, Tf = 0, dt = None):
     # Inputs: ['ref', 'sens']
@@ -50,19 +50,19 @@ def PID2(Kp = 1, Ki = 0.0, Kd = 0, b = 1, c = 1, Tf = 0, dt = None):
     return sys
 
 # Attitude Controller Models
-sysAlt = PID2(0.500, Ki = 0.050, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
+sysAlt = PID2(0.200, Ki = 0.050, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
 sysAlt.InputName = ['refAlt', 'sensAlt']
 sysAlt.OutputName = ['refAltRate']
 
-sysPhi = PID2(0.500, Ki = 0.050, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
+sysPhi = PID2(0.100, Ki = 0.000, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
 sysPhi.InputName = ['refPhi', 'sensPhi']
 sysPhi.OutputName = ['refP']
 
-sysTheta = PID2(0.500, Ki = 0.050, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
+sysTheta = PID2(0.100, Ki = 0.000, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
 sysTheta.InputName = ['refTheta', 'sensTheta']
 sysTheta.OutputName = ['refQ']
 
-sysPsi = PID2(0.500, Ki = 0.050, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
+sysPsi = PID2(0.100, Ki = 0.020, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
 sysPsi.InputName = ['refPsi', 'sensPsi']
 sysPsi.OutputName = ['refR']
 
@@ -73,19 +73,19 @@ sysAtt.OutputName = sysAlt.OutputName + sysPhi.OutputName + sysTheta.OutputName 
 
 
 # SCAS Controller Models
-sysAltRate = PID2(0.100, Ki = 0.020, Kd = 0.010, b = 1, c = 0, Tf = tFrameRate_s)
+sysAltRate = PID2(0.200, Ki = 0.050, Kd = 0.010, b = 1, c = 0, Tf = tFrameRate_s)
 sysAltRate.InputName = ['refAltRate', 'sensAltRate']
 sysAltRate.OutputName = ['cmdHeave']
 
-sysP = PID2(0.150, Ki = 0.050, Kd = 0.003, b = 1, c = 0, Tf = tFrameRate_s)
+sysP = PID2(0.150, Ki = 0.050, Kd = 0.030, b = 1, c = 1, Tf = tFrameRate_s)
 sysP.InputName = ['refP', 'sensP']
 sysP.OutputName = ['cmdP']
 
-sysQ = PID2(0.150, Ki = 0.050, Kd = 0.003, b = 1, c = 0, Tf = tFrameRate_s)
+sysQ = PID2(0.150, Ki = 0.050, Kd = 0.030, b = 1, c = 0, Tf = tFrameRate_s)
 sysQ.InputName = ['refQ', 'sensQ']
 sysQ.OutputName = ['cmdQ']
 
-sysR = PID2(0.300, Ki = 0.100, Kd = 0.000, b = 1, c = 0, Tf = tFrameRate_s)
+sysR = PID2(0.300, Ki = 0.100, Kd = 0.030, b = 1, c = 0, Tf = tFrameRate_s)
 sysR.InputName = ['refR', 'sensR']
 sysR.OutputName = ['cmdR']
 
@@ -98,10 +98,10 @@ sysScas.OutputName = sysAltRate.OutputName + sysP.OutputName + sysQ.OutputName +
 
 # Mixer
 ctrlEff = 0.25 * np.array([
-    [ 1.0, 1.0, 1.0, 1.0],
-    [-1.0, 1.0, 1.0,-1.0],
-    [ 1.0,-1.0, 1.0,-1.0],
-    [ 1.0, 1.0,-1.0,-1.0]])
+    [ 1.0, 1.0, 1.0, 1.0], # Heave
+    [-1.0, 1.0, 1.0,-1.0], # Poll
+    [ 1.0,-1.0, 1.0,-1.0], # Pitch
+    [ 1.0, 1.0,-1.0,-1.0]]) # Yaw
 
 mixSurf = np.linalg.pinv(ctrlEff)
 mixSurf [abs(mixSurf) / np.max(abs(mixSurf)) < 0.05] = 0.0
@@ -110,13 +110,13 @@ mixSurf [abs(mixSurf) / np.max(abs(mixSurf)) < 0.05] = 0.0
 #%%
 ## Load Sim
 model = 'F450'
-sim = JSBSimWrap(model, dt = 1/500)
+sim = JSBSimWrap(model, dt = 1/200)
 sim.SetupIC('initGrnd.xml')
 sim.SetupOutput()
 sim.DispOutput()
 sim.RunTrim()
 
-sim.SetTurb(turbType = 4, turbSeverity = 3, vWind20_mps = 6.0, vWindHeading_deg = 0.0)
+sim.SetTurb(turbType = 4, turbSeverity = 3, vWind20_mps = 2.0, vWindHeading_deg = 0.0)
 
 #%%
 ft2m = 0.3048
@@ -189,19 +189,25 @@ for t_s in tSamp_s:
     excCmdR = 0
 
     # Psi Step
-    if t_s >= 10 :
-        refPsi = 45.0 *np.pi/180.0
+#    if t_s >= 10 :
+#        refPsi = 45.0 *np.pi/180.0
 
     # Phi Step
     if t_s >= 20 and t_s < 30 :
-        refPhi = 20 *np.pi/180.0
+        refPhi = 30 *np.pi/180.0
+        
+    # Roll Doublet
+#    if t_s >= 15 and t_s < 17 :
+#        excRefP = 5 *np.pi/180.0
+#    if t_s >= 17 and t_s < 19 :
+#        excRefP = -5 *np.pi/180.0
 
     ## Run Scas
     if t_s < 1.0:
         yMixer = np.array([0, 0, 0, 0])
 
     elif t_s >= 1.0 and t_s < 5:
-        refAltRate = 3.0
+        refAltRate = 10.0
 
         inScas = np.array([refAltRate, sensAltRate, 0, 0, 0, 0, 0, 0])
         tScas, yScas, xScas = control.forced_response(sysScas, T = tStep, U = np.array([inScas, inScas]).T, X0 = xScas[:,-1])
@@ -215,7 +221,7 @@ for t_s in tSamp_s:
 #        sensAlt0 = sensAlt
 
     elif t_s >= 5:
-        refAlt = sensAlt0 + 50 * ft2m
+        refAlt = sensAlt0 + 100 * ft2m
         refPhi = refPhi
         refTheta = refTheta
         refPsi = refPsi
@@ -230,7 +236,7 @@ for t_s in tSamp_s:
             refQ = yAtt[2, -1] + excRefQ
             refR = yAtt[3, -1] + excRefR
         else:
-            refAltRate = 2.0
+            refAltRate = 0.0
             refP = excRefP
             refQ = excRefQ
             refR = excRefR
@@ -297,7 +303,7 @@ plt.plot(tSamp_s, np.array(sensRList) * 180/np.pi)
 plt.show()
 
 #%%
-yArray = np.array(yList) * 50000
+yArray = np.array(yList) * 16000
 posArray = np.array(posMotorList)
 
 plt.figure(2)
